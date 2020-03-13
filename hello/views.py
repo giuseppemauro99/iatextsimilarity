f3cfd12e53a8263e7fd366f49f3e27f1afc8c8c8
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-03-13 16:52:38 +0100

    added form

--- hello/views.py
+++ hello/views.py
@@ -6,23 +6,22 @@ from .forms import Form1
 
 # Create your views here.
 def index(request):
-    def get_name(request):
-        # if this is a POST request we need to process the form data
-        if request.method == 'POST':
-            # create a form instance and populate it with data from the request:
-            form = Form1(request.POST)
-            # check whether it's valid:
-            if form.is_valid():
-                # process the data in form.cleaned_data as required
-                # ...
-                # redirect to a new URL:
-                return HttpResponseRedirect('/calculatesimilarity/')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = Form1(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/calculatesimilarity/')
 
-        # if a GET (or any other method) we'll create a blank form
-        else:
-            form = Form1()
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = Form1()
 
-        return render(request, 'index.html', {'form': form})
+    return render(request, 'index.html', {'form': form})
 
 
 def db(request):
