44cb9ba51d971629a5c660e16baa0f2f6b89e232
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-03-13 16:42:33 +0100

    added form

---
+++ hello/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class  Form1 ( forms.Form ):
+     sentence1  =  forms.CharField( label = 'Frase1' )
+     sentence2 = forms.CharField(label='Frase2')
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import Greeting
+from .forms import Form1
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    def get_name(request):
+        # if this is a POST request we need to process the form data
+        if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = Form1(request.POST)
+            # check whether it's valid:
+            if form.is_valid():
+                # process the data in form.cleaned_data as required
+                # ...
+                # redirect to a new URL:
+                return HttpResponseRedirect('/calculatesimilarity/')
+
+        # if a GET (or any other method) we'll create a blank form
+        else:
+            form = Form1()
+
+        return render(request, 'index.html', {'form': form})
 
 
 def db(request):
