c93bf6ce885cebc70a9f268510324092a9bce032
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-03-13 18:37:28 +0100

    fixing

--- hello/forms.py
+++ hello/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class  Form1 ( forms.Form ):
-     file1 = forms.FileField(label="Inserisci file 1",upload_to='uploads/')
-     file2_query = forms.FileField(label="Inserisci file 2",upload_to='uploads/')
+     file1 = forms.FileField(label="Inserisci file 1")
+     file2_query = forms.FileField(label="Inserisci file 2")
