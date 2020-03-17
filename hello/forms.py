7e6a42e7edf0538d5b6901293df1460e24bd4eb7
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-03-17 15:55:18 +0100

    adding label

--- hello/forms.py
+++ hello/forms.py
@@ -4,3 +4,14 @@ from django import forms
 class  Form1 ( forms.Form ):
      file1 = forms.FileField(label="Inserisci file 1")
      file2_query = forms.FileField(label="Inserisci file 2")
+     label1 = forms.CharField(label="Nome etichetta")
+     label1_interval = forms.CharField(label='Intervallo',
+                         widget=forms.TextInput(attrs={'placeholder': 'Intervallo etichetta, es. 0.0-12.5'}))
+     label2 = forms.CharField(label="Nome etichetta")
+     label2_interval = forms.CharField(label='Intervallo',
+                                       widget=forms.TextInput(
+                                            attrs={'placeholder': 'Intervallo etichetta, es. 0.0-12.5'}))
+     label3 = forms.CharField(label="Nome etichetta")
+     label3_interval = forms.CharField(label='Intervallo',
+                                       widget=forms.TextInput(
+                                            attrs={'placeholder': 'Intervallo etichetta, es. 0.0-12.5'}))
