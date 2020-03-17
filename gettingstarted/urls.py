43ca469f99df1da93cfc8419a7166c1c07060ece
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-03-17 18:00:39 +0100

    fixing

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
-    path("calculatesimilarity/", hello.views.calculatesimilarity, name="calculatesimilarity")
+    path("calculatesimilarity/", hello.views.calculatesimilarity, name="calculatesimilarity"),
+    path("download_csv/", hello.views.download_csv, name="download_csv")
 ]
