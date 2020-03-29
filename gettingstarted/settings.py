77dbe267ec97a2e462b209b02a0d5b8b62dee53c
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-03-29 18:11:05 +0200

    Dockerizing app

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django',
+        'ENFORCE_SCHEMA': True,
+        'NAME': 'django_mongodb_docker',
+        'HOST': 'mongodb',
+        'PORT': 27017,
+        'USER': 'root',
+        'PASSWORD': 'mongoadmin',
+        'AUTH_SOURCE': 'admin',
+        'AUTH_MECHANISM': 'SCRAM-SHA-1',
     }
 }
 
