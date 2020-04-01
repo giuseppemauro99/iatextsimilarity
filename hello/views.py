501842a1b0d69b1d4d8503205c0028e975c59249
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-04-01 21:13:00 +0200

    fixing uri string to mongodb

--- hello/views.py
+++ hello/views.py
@@ -15,7 +15,7 @@ from .models import Greeting
 from .forms import Form1
 
 #MONGO_DB_URI = "mongodb://heroku_166t21vc:chbme62a0ama4gda9p203bgs0e@ds113935.mlab.com:13935/heroku_166t21vc?retryWrites=false"
-MONGO_DB_URI = "mongodb://"+os.environ.get("MONGO_INITDB_ROOT_USERNAME")+":"+os.environ.get("MONGO_INITDB_ROOT_PASSWORD")+"@"+"mongodb"+":27017"
+MONGO_DB_URI = "mongodb://"+os.environ.get("MONGODB_USERNAME")+":"+os.environ.get("MONGODB_PASSWORD")+"@"+"mongodb"+":27017"
 
 myclient = pymongo.MongoClient(MONGO_DB_URI)
 #mydb = myclient["heroku_166t21vc"]
