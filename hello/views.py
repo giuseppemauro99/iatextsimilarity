c5dc6ac1be400b23783def38bdc787bace819c9d
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-04-03 00:25:22 +0200

    fixing

--- hello/views.py
+++ hello/views.py
@@ -14,6 +14,7 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from .models import Greeting
 from .forms import Form1
 
+
 #MONGO_DB_URI = "mongodb://heroku_166t21vc:chbme62a0ama4gda9p203bgs0e@ds113935.mlab.com:13935/heroku_166t21vc?retryWrites=false"
 MONGO_DB_URI = "mongodb://"+"user"+":"+"mongoadmin"+"@"+"mongodb"+":27017"
 
