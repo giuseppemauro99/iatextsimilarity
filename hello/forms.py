9071adaebad5848ba1a581a096f7534e57780772
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-04-03 02:06:28 +0200

    added colours to output

--- hello/forms.py
+++ hello/forms.py
@@ -1,17 +1,21 @@
 from django import forms
 
 
-class Form1 (forms.Form):
-     file1 = forms.FileField(label="Inserisci file 1")
-     file2_query = forms.FileField(label="Inserisci file 2")
-     label1 = forms.CharField(label="Nome etichetta")
-     label1_interval = forms.CharField(label='Intervallo',
-                         widget=forms.TextInput(attrs={'placeholder': 'Intervallo etichetta, es. 0.0-20.5'}))
-     label2 = forms.CharField(label="Nome etichetta")
-     label2_interval = forms.CharField(label='Intervallo',
-                                       widget=forms.TextInput(
-                                            attrs={'placeholder': 'Intervallo etichetta, es. 20.5-50.0'}))
-     label3 = forms.CharField(label="Nome etichetta")
-     label3_interval = forms.CharField(label='Intervallo',
-                                       widget=forms.TextInput(
-                                            attrs={'placeholder': 'Intervallo etichetta, es. 50.0-100.0'}))
+class Form1(forms.Form):
+    file1 = forms.FileField(label="Inserisci file 1")
+    file2_query = forms.FileField(label="Inserisci file 2")
+    label1 = forms.CharField(label="Nome etichetta")
+    label1_interval = forms.CharField(label='Intervallo',
+                                      widget=forms.TextInput(
+                                          attrs={'placeholder': 'Intervallo etichetta, es. 0.0-20.5',
+                                                 'min-width': '30%'}))
+    label2 = forms.CharField(label="Nome etichetta")
+    label2_interval = forms.CharField(label='Intervallo',
+                                      widget=forms.TextInput(
+                                          attrs={'placeholder': 'Intervallo etichetta, es. 20.5-50.0',
+                                                 'min-width': '30%'}))
+    label3 = forms.CharField(label="Nome etichetta")
+    label3_interval = forms.CharField(label='Intervallo',
+                                      widget=forms.TextInput(
+                                          attrs={'placeholder': 'Intervallo etichetta, es. 50.0-100.0',
+                                                 'min-width': '30%'}))
