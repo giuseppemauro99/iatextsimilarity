de090925c124887f39a578693f3b0fdbff134dd4
Giuseppe Mauro <giuseppemauro99@gmail.com>
2020-04-20 16:35:30 +0200

    fix

--- hello/views.py
+++ hello/views.py
@@ -115,7 +115,7 @@ def similarityMatrix(file1, file2, request):
             doc2 = nlp(line2)
             val = f"{(doc1.similarity(doc2) * 100) :.2f}"  # calcolo la similarità, la trasformo in percentuale e prendo solo 2 cifre decimali
             sim_matrix[i][j] = val2Label(val, request)
-            sim_matrix[i][j] = sim_matrix[i][j] + " " + "("+str(i)+","+str(j)+")"
+            sim_matrix[i][j] = sim_matrix[i][j] + " " + "("+str(i+1)+","+str(j+1)+")"
             j = j + 1
         i = i + 1
 
